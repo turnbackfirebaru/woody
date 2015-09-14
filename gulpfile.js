94994d07dcb0e1ab364e82843073014fea28e4de
Felix Schlitter <felixschlitter@gmail.com>
2015-09-15 11:07:01 +1200

    Add `PhantomJS`

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp')
   , browserify = require('browserify')
   , babel = require('gulp-babel');
 
-gulp.task('compile', function () {
+gulp.task('make:node', function () {
   return gulp.src(['src/**/*.js'])
     .pipe(sourcemaps.init())
     .pipe(babel())
@@ -14,15 +14,17 @@ gulp.task('compile', function () {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('browserify', ['compile'], function() {
+gulp.task('make:browser', ['make:node'], function() {
   return browserify({ entries: './index.js' })
     .bundle()
     .pipe(source('woody.js'))
     .on('error', function(err) {
       console.log('Error:', err.message);
     })
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./'));
 });
+
+gulp.task('default', ['make:browser']);
 
 gulp.task('watch', ['default'], function () {
   gulp.watch('./src/**/*', ['default']);
